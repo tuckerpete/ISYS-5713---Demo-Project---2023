3c362bc0254b0bbdb15e728161af861869698bbf
tuckerpete <47702843+tuckerpete@users.noreply.github.com>
2023-09-22 22:28:00 -0500

    removed create_database() from base_model

--- models/base_model.py
+++ models/base_model.py
@@ -3,16 +3,5 @@
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy import create_engine
 
-engine = None
-
 class Base(DeclarativeBase):
     pass
-
-def create_database():
-
-    # Create an engine
-    global engine # this means we can now use "engine" from the global context where we defined it above
-    engine = create_engine("sqlite:///models/cache.sqlite", echo=True)
-
-    # Build tables
-    Base.metadata.create_all(engine)
